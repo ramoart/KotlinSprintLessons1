30e50ba4a1bf7b62b31e6db0c227e13c29ec1f7b
ramoart <donni.lab.1@gmail.com>
2025-07-24 14:12:00 +0200

    KSL1_lesson1_task2 completed

--- src/main/kotlin/lesson1_task2.kt
+++ src/main/kotlin/lesson1_task2.kt
@@ -1,7 +1,7 @@
 package org.example
 
 fun main() {
-    var countOrder: Short = 75
+    val countOrder: Short = 75
     val gratitudeText: String = "Благодарим вас за покупку!"
     println(countOrder)
     println(gratitudeText)
