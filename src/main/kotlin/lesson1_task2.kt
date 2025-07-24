cf23ffdc04dedd380a86402d1e0b7306d908e89c
ramoart <donni.lab.1@gmail.com>
2025-07-24 08:42:28 +0200

    Lesson 1 Task 2 complete

---
+++ src/main/kotlin/lesson1_task2.kt
@@ -0,0 +1,13 @@
+package org.example
+
+fun main() {
+    var countOrder: Short = 75
+    val gratitudeText: String = "Благодарим вас за покупку!"
+    println(countOrder)
+    println(gratitudeText)
+
+    var countEmployers: Short = 2000
+    //println(countEmployers)
+    countEmployers = 1999
+    println(countEmployers)
+}
