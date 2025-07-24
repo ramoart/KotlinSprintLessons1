116b865d0c3d46c8edf6bf66127028427f630c6f
ramoart <donni.lab.1@gmail.com>
2025-07-24 09:03:48 +0200

    Lesson 1 Task 3 complete

---
+++ src/main/kotlin/lesson1_task3.kt
@@ -0,0 +1,14 @@
+package org.example
+
+fun main(){
+    val year: Short = 1961
+    var hour: String = "09"
+    var minute: String = "07"
+    println(year)
+    println(hour)
+    println(minute)
+
+    hour = "10"
+    minute = "55"
+    println("Посадка - $hour:$minute")
+}
