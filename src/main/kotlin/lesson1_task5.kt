a1d6153553e401b0a4649d85b843312b0ce2d9c0
ramoart <donni.lab.1@gmail.com>
2025-07-25 10:06:09 +0200

    KS-1-5 completed

--- src/main/kotlin/lesson1_task5.kt
+++ src/main/kotlin/lesson1_task5.kt
@@ -1,10 +1,12 @@
 package org.example
+const val secondsInHour: Int = 3600
+const val secondsInMinute: Int = 60
 
 fun main() {
     val secondsAll: Int = 6480
-    val hours: Int = secondsAll / 3600
-    val minutes: Int = (secondsAll - 3600) / 60
-    val secondsRest: Int = secondsAll - 3600 - 2880
-    val formattedTime: String = String.format("%02d:%02d:%02d", hours, minutes, secondsRest)
+    val hours: Int = secondsAll / secondsInHour
+    val minutes: Int = (secondsAll - secondsInHour) / secondsInMinute
+    val secondsRest: Int = secondsAll - secondsInHour - minutes*secondsInMinute
+    val formattedTime: String = "%02d:%02d:%02d".format(hours, minutes, secondsRest)
     println("Время в космосе - $formattedTime")
 }
