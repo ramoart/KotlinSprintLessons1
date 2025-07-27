d2a9af24f1274d5ec51d1186bf2be02d50843516
ramoart <donni.lab.1@gmail.com>
2025-07-27 20:59:54 +0200

    KS-2-5 completed, 27.07.25

--- src/main/kotlin/lesson2/lesson2_task5.kt
+++ src/main/kotlin/lesson2/lesson2_task5.kt
@@ -2,12 +2,14 @@ package lesson2
 
 import kotlin.math.pow
 
+const val ONE_INT = 1
+
 fun main() {
     val years = 20
     val moneyPut = 70000
     val percentRate = 0.167f
 
-    val summaryMoney = moneyPut * (1 + percentRate).pow(years)
+    val summaryMoney = moneyPut * (ONE_INT + percentRate).pow(years)
     val formattedSummaryMoney = "%.3f".format(summaryMoney)
 
     println("Ваш размер вклада через $years лет составит: $formattedSummaryMoney")
