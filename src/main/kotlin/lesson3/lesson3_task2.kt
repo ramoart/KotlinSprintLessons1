cdd1f8d9a5181a99114afc170bc89376f7085d2e
ramoart <donni.lab.1@gmail.com>
2025-07-28 13:50:08 +0200

    KS-3-2 completed, Rev-28.07.25

---
+++ src/main/kotlin/lesson3/lesson3_task2.kt
@@ -0,0 +1,13 @@
+package lesson3
+
+fun main() {
+    val lastNameMaiden = "Андреева"
+    val firstName = "Татьяна"
+    val middleName = "Сергеевна"
+    val lastName = "Сидорова"
+    val ageStart = 20
+    val ageEnd = 22
+
+    println("[$lastNameMaiden $firstName $middleName, $ageStart]")
+    println("[$lastName $firstName $middleName, $ageEnd]")
+}
