754df1ca281c7d4e943b32177a93fbf4db34e9ca
ramoart <donni.lab.1@gmail.com>
2025-07-28 12:45:37 +0200

    KS-2-5 completed, Rev-28.07.25

--- src/main/kotlin/lesson2/lesson2_task5.kt
+++ src/main/kotlin/lesson2/lesson2_task5.kt
@@ -3,11 +3,13 @@ package lesson2
 import kotlin.math.pow
 
 const val ONE_INT = 1
+const val TO_FRACTION = 100
 
 fun main() {
+    val percentRateDouble = 16.7
+    val percentRate = percentRateDouble / TO_FRACTION
     val years = 20
     val moneyPut = 70000
-    val percentRate = 0.167f
 
     val summaryMoney = moneyPut * (ONE_INT + percentRate).pow(years)
     val formattedSummaryMoney = "%.3f".format(summaryMoney)
