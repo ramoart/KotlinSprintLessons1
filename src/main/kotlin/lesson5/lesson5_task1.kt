d47a06ab9be3bbf2774da1af7db817cb235e2876
ramoart <donni.lab.1@gmail.com>
2025-07-30 15:10:03 +0200

    KS-5-1 completed

---
+++ src/main/kotlin/lesson5/lesson5_task1.kt
@@ -0,0 +1,13 @@
+package lesson5
+
+fun main() {
+    println("DeepThoughtCAPTCHA: введите целочисленный результат сложения двух чисел 24 + 18 =")
+    val inputData = readLine()?.toInt()
+    val outputData = if (inputData == 42) {
+        "Добро пожаловать!"
+    } else {
+        "Доступ запрещен"
+    }
+
+    println(outputData)
+}
