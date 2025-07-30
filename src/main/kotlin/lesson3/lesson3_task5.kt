966f56dc0f87f598a94f640b36cd64d79dc2519d
ramoart <donni.lab.1@gmail.com>
2025-07-30 08:27:56 +0200

    KS-3-5 completed, Rev-30.07.25

--- src/main/kotlin/lesson3/lesson3_task5.kt
+++ src/main/kotlin/lesson3/lesson3_task5.kt
@@ -2,12 +2,10 @@ package lesson3
 
 fun main() {
     val getData = "D2-D4;0"
-    val splittedData = getData.split(";")
-    val moveData = splittedData[0]
-    val numberMove = splittedData[1]
-    val partsMove = moveData.split("-")
-    val firstMove = partsMove[0]
-    val secondMove = partsMove[1]
+    val splittedData = getData.split("-", ";")
+    val firstMove = splittedData[0]
+    val secondMove = splittedData[1]
+    val numberMove = splittedData[2]
 
     println(firstMove)
     println(secondMove)
