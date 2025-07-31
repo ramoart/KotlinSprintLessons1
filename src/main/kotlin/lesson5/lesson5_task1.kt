ffdae790cc9a20809977a53bbbbeb5800b9577c5
ramoart <donni.lab.1@gmail.com>
2025-07-31 09:17:04 +0200

    KS-5-1 completed, Rev-31.07.25

--- src/main/kotlin/lesson5/lesson5_task1.kt
+++ src/main/kotlin/lesson5/lesson5_task1.kt
@@ -2,7 +2,7 @@ package lesson5
 
 fun main() {
     println("DeepThoughtCAPTCHA: введите целочисленный результат сложения двух чисел 24 + 18 =")
-    val inputData = readLine()?.toInt()
+    val inputData = readln().toInt()
     val outputData = if (inputData == 42) {
         "Добро пожаловать!"
     } else {
