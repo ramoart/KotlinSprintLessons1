bfb8d4babcd17733601e4091768c2c24f18ac246
ramoart <donni.lab.1@gmail.com>
2025-07-31 21:07:57 +0200

    KS-5-1 completed, Rev-31.07.25

--- src/main/kotlin/lesson5/lesson5_task1.kt
+++ src/main/kotlin/lesson5/lesson5_task1.kt
@@ -1,9 +1,11 @@
 package lesson5
 
 fun main() {
-    println("DeepThoughtCAPTCHA: введите целочисленный результат сложения двух чисел 24 + 18 =")
-    val inputData = readln().toInt()
-    val outputData = if (inputData == 42) {
+    println("Для входа в систему, сложите два целочисленных числа, поочередно вводя данные и затем напишите результат сложениw")
+    val inputDataA = readln().toInt()
+    val inputDataB = readln().toInt()
+    val inputDataC = readln().toInt()
+    val outputData = if (inputDataC == inputDataA + inputDataB) {
         "Добро пожаловать!"
     } else {
         "Доступ запрещен"
