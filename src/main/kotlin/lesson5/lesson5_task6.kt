2191e5ef219d8bc189217329285b21f82791a19f
ramoart <donni.lab.1@gmail.com>
2025-08-01 06:56:34 +0200

    KS-5-6 completed, Rev-01.08.25

---
+++ src/main/kotlin/lesson5/lesson5_task6.kt
@@ -0,0 +1,24 @@
+package lesson5
+
+import java.lang.Math.pow
+import kotlin.math.pow
+
+fun main() {
+    println("Для расчета вашего ИМТ введите по очередно ваш вес (кг) и рост (см):")
+    val weightUser = readln().toDouble()
+    val heightUser = readln().toDouble()
+    val imtCalc = weightUser / (heightUser / IN_METERS).pow(2)
+    val formattedImt = "%f.2".format(imtCalc)
+    when {
+        imtCalc < LOW_IMT -> println("Недостаточная масса тела, ваш ИМТ: $formattedImt")
+        imtCalc in LOW_IMT..NORMAL_IMT -> println("Нормальная масса тела, ваш ИМТ: $formattedImt")
+        imtCalc in NORMAL_IMT..HIGH_IMT -> println("Избыточная масса тела, ваш ИМТ: $formattedImt")
+        imtCalc > HIGH_IMT -> println("Ожирение, ваш ИМТ: $formattedImt")
+    }
+
+}
+
+const val IN_METERS = 100
+const val LOW_IMT = 18.5
+const val NORMAL_IMT: Double = 25.0
+const val HIGH_IMT: Double = 30.0
