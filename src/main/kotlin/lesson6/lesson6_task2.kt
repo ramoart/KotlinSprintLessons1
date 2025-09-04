272933ef48d3a9c7434e7370dd38244d9d019781
ramoart <donni.lab.1@gmail.com>
2025-09-04 14:42:33 +0200

    KS-6-2 completed, Rev-04.09.25

--- src/main/kotlin/lesson6/lesson6_task2.kt
+++ src/main/kotlin/lesson6/lesson6_task2.kt
@@ -6,11 +6,9 @@ import kotlin.concurrent.thread
 fun main() {
     println("Введите количество секунд, на которое необходимо установить таймер:")
     val inputSeconds = readln().toInt()
-    var timeCounter = inputSeconds
-
-    while (timeCounter > 0) {
-        println("Осталось ${timeCounter--} секунд")
-        Thread.sleep(1000)
-    }
-    if (timeCounter == 0) println("Прошло $inputSeconds секунд")
+    val secondsLong = (inputSeconds * RATIO_TO_MILLIS).toLong()
+    Thread.sleep(secondsLong)
+    println("Прошло $inputSeconds секунд")
 }
+
+const val RATIO_TO_MILLIS = 1000
