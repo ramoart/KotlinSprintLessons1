f004cbc10c1e4c17fff6ad437cfb604244710a59
ramoart <donni.lab.1@gmail.com>
2025-09-07 13:36:25 +0200

    KS-7-2 completed

--- src/main/kotlin/lesson7/lesson7_task2.kt
+++ src/main/kotlin/lesson7/lesson7_task2.kt
@@ -3,19 +3,15 @@ package lesson7
 import kotlin.random.Random
 
 fun main() {
-    var randomNumber = Random.nextInt(1000, 9999)
-    println("Ваш код авторизации: $randomNumber")
-    println("Введит код авторизации:")
-    var inputNumber = readln().toInt()
-
     do {
-        if (inputNumber != randomNumber) {
-            println("Код введен неверно!")
-            randomNumber = Random.nextInt(1000, 9999)
-            println("Ваш код авторизации: $randomNumber")
-            println("Введит код авторизации:")
-            inputNumber = readln().toInt()
-        }
-        if (inputNumber == randomNumber) println("Добро пожаловать, код авторизации введен верно.")
+        val randomNumber = Random.nextInt(1000, 9999)
+        println("Ваш код авторизации: $randomNumber")
+        println("Введите код авторизации:")
+        val inputNumber = readln().toInt()
+
+        if (inputNumber != randomNumber) println("Код введен неверно!")
+
     } while (inputNumber != randomNumber)
+
+    println("Добро пожаловать, код авторизации введен верно.")
 }
