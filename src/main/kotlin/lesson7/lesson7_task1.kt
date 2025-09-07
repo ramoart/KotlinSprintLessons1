35c5d3a2376b2260255ff9d0ad7c0638b3c29647
ramoart <donni.lab.1@gmail.com>
2025-09-07 14:54:34 +0200

    KS-7-1 completed

--- src/main/kotlin/lesson7/lesson7_task1.kt
+++ src/main/kotlin/lesson7/lesson7_task1.kt
@@ -1,19 +1,13 @@
 package lesson7
 
-import kotlin.random.Random
-
-
 fun main() {
     println("Добро пожаловать в генератор паролей!")
     val rangeChar = 'a'..'z'
     val rangeInt = 0..9
-
-    val randomChar = mutableListOf<Char>()
-    val randomInt = mutableListOf<Int>()
+    var password = ""
 
     repeat(3) {
-        randomChar.add(rangeChar.random())
-        randomInt.add(rangeInt.random())
+        password += rangeChar.random() + rangeInt.random().toString()
     }
-    println("Ваш пароль: ${randomChar[0]}${randomInt[0]}${randomChar[1]}${randomInt[1]}${randomChar[2]}${randomInt[2]}")
+    println("Ваш пароль: $password")
 }
