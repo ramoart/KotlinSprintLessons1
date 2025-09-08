5710942aeef16794af4197e639a821cec8e085e0
ramoart <donni.lab.1@gmail.com>
2025-09-08 17:30:38 +0200

    KS-8-1 completed

--- src/main/kotlin/lesson8/lesson8_task1.kt
+++ src/main/kotlin/lesson8/lesson8_task1.kt
@@ -1,14 +1,11 @@
 package lesson8
 
 fun main() {
-    var viewsOfDay: Int
     val countArray = Array(DAYS_IN_WEEK) { 0 }
 
     for (i in 0..COEFFICIENT) {
-        viewsOfDay = (0..99).random()
-        countArray[i] = viewsOfDay
+        countArray[i] = (0..99).random()
     }
-    countArray[1]
     println("Количество просмотров за неделю: ${countArray.sum()}")
 }
 
