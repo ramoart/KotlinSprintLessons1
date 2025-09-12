5439280bd881c77030fcce6aab0f93b7b14eb582
ramoart <donni.lab.1@gmail.com>
2025-09-12 09:57:27 +0200

    KS-9-5 has been completed, please approve the pull request

---
+++ src/main/kotlin/lesson9/lesson9_task5.kt
@@ -0,0 +1,15 @@
+package lesson9
+
+fun main() {
+    val burgerList = mutableListOf<String>()
+    println("Введите названия пяти ингредиентов:")
+    repeat(COUNT_OF_PIECES) {
+        burgerList.add(readln())
+    }
+
+    burgerList.sort()
+    burgerList[0] = burgerList[0].replaceFirstChar { it.uppercase() }
+    println("Ингредиенты блюда: ${burgerList.joinToString(", ")}")
+}
+
+const val COUNT_OF_PIECES = 5
