b73a662a6f458a2688ffa7ec0fd69b37bf0e7d13
ramoart <donni.lab.1@gmail.com>
2025-09-12 07:46:43 +0200

    KS-9-3 has been completed, please approve the pull request

--- src/main/kotlin/lesson9/lesson9_task3.kt
+++ src/main/kotlin/lesson9/lesson9_task3.kt
@@ -5,5 +5,10 @@ fun main() {
     println("Введите количество порций:")
     val inputPieces = readln().toInt()
     val usersIngredients = countIngredients.map { it * inputPieces }
-    println("На $inputPieces порций вам понадобится: Яиц - ${usersIngredients[0]}, молока - ${usersIngredients[1]}, сливочного масла - ${usersIngredients[2]}")
+    println(
+        "На $inputPieces порций вам понадобится: " +
+                "Яиц - ${usersIngredients[0]}, " +
+                "молока - ${usersIngredients[1]}, " +
+                "сливочного масла - ${usersIngredients[2]}"
+    )
 }
