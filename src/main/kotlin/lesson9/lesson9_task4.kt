f7a5e6a29482a5b0d3a2ecb6e51237cbf02fc054
ramoart <donni.lab.1@gmail.com>
2025-09-14 08:39:57 +0200

    KS-9-4 has been completed, please approve the pull request

--- src/main/kotlin/lesson9/lesson9_task4.kt
+++ src/main/kotlin/lesson9/lesson9_task4.kt
@@ -2,9 +2,7 @@ package lesson9
 
 fun main() {
     println("Введите 5 ингредиентов, перечислив их через запятую:")
-    val inputItems = readln()
-    val burgerList = inputItems.split(", ").toMutableList()
-    val burgerSorted = burgerList.sorted()
+    val burgerSorted = readln().split(", ").sorted()
 
     println("Отсортированный список ингредиентов: $burgerSorted")
 }
