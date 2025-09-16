79c6c2f223cd6234ecb859a57c96b35cca90b37b
ramoart <donni.lab.1@gmail.com>
2025-09-16 07:39:47 +0200

    KS-9-5 has been completed, please approve the pull request

--- src/main/kotlin/lesson9/lesson9_task5.kt
+++ src/main/kotlin/lesson9/lesson9_task5.kt
@@ -4,13 +4,13 @@ fun main() {
     val burgerList = mutableListOf<String>()
     println("Введите названия пяти ингредиентов:")
     repeat(COUNT_OF_PIECES) {
-         burgerList.add(readln())
+        burgerList.add(readln())
     }
 
     burgerList.sort()
     burgerList[0] = burgerList[0].replaceFirstChar { it.uppercase() }
-    val burgerSet = java.util.TreeSet(burgerList)
-    println("Ингредиенты блюда: ${burgerSet.joinToString(", ")}")
+
+    println("Ингредиенты блюда: ${burgerList.toSortedSet().joinToString(", ")}")
 }
 
 const val COUNT_OF_PIECES = 5
