ecf0b9227adb302475cbf5c23d747869cf714dc7
ramoart <donni.lab.1@gmail.com>
2025-09-17 07:46:26 +0200

    KS-10-4 has been completed, please approve the pull request.

--- src/main/kotlin/lesson10/lesson10_task_4.kt
+++ src/main/kotlin/lesson10/lesson10_task_4.kt
@@ -1,35 +1,26 @@
-package lesson10
-
 fun main() {
-    var resultOfUser = resultsOfThrow()
-    var resultOfComputer = resultsOfThrow()
     var counterOfWin = 0
-
-    counterOfWin = round(resultOfUser, resultOfComputer, counterOfWin)
     var inputAnswer: String
+
     do {
+        val resultOfUser = throwDice()
+        val resultOfComputer = throwDice()
+        counterOfWin = startRound(resultOfUser, resultOfComputer, counterOfWin)
+
         println("Хотите бросить кости еще раз? Введите Да или Нет")
         inputAnswer = readln()
-        if (inputAnswer.equals("Да", ignoreCase = true)) {
-            resultOfUser = resultsOfThrow()
-            resultOfComputer = resultsOfThrow()
-            counterOfWin = round(resultOfUser, resultOfComputer, counterOfWin)
-        } else {
-            println("Количество побед $counterOfWin")
-        }
     } while (inputAnswer.equals("Да", ignoreCase = true))
 
+    println("Количество побед: $counterOfWin")
 }
 
-fun resultsOfThrow(): Int {
-    val resultsOfThrow = (MIN_VALUE..MAX_VALUE).random()
-    return resultsOfThrow
-}
+fun throwDice() = (MIN_VALUE..MAX_VALUE).random()
 
-fun round(resultOfUser: Int, resultOfComputer: Int, counterOfWin: Int): Int {
+fun startRound(resultOfUser: Int, resultOfComputer: Int, counterOfWin: Int): Int {
     var counter = counterOfWin
-    println("Игрок бросил кость, выпавшее значение на кубике: $resultOfUser")
-    println("Компьютер бросил кость, выпавшее значение на кубике: $resultOfComputer")
+    println("Игрок бросил кость: $resultOfUser")
+    println("Компьютер бросил кость: $resultOfComputer")
+
     when {
         resultOfUser > resultOfComputer -> {
             println("Победило человечество")
@@ -39,6 +30,7 @@ fun round(resultOfUser: Int, resultOfComputer: Int, counterOfWin: Int): Int {
         resultOfComputer > resultOfUser -> println("Победила машина")
         else -> println("Победила дружба")
     }
+
     return counter
 }
 
