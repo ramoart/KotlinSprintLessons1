57d67fbfada7c97d86dca70da861819397718e41
ramoart <donni.lab.1@gmail.com>
2025-09-17 07:40:58 +0200

    KS-10-3 has been completed, please approve the pull request.

--- src/main/kotlin/lesson10/lesson10_task3.kt
+++ src/main/kotlin/lesson10/lesson10_task3.kt
@@ -3,21 +3,21 @@ package lesson10
 fun main() {
     println("Задайте длину пароля для генерации:")
     val inputLengthPassword = readln().toInt()
-    generatePassword(inputLengthPassword)
+    val passwordList = generatePassword(inputLengthPassword)
+    println("Ваш пароль: ${passwordList.joinToString("")}")
 }
 
-fun generatePassword(inputLengthPassword: Int) {
+fun generatePassword(inputLengthPassword: Int): List<Char> {
     val numbers = '0'..'9'
-    val symbols = "!\"#\$%&'()*+,-./ "
-    val specialCharArray = symbols.toCharArray()
+    val symbols = ' '..'/'
     val passwordList = mutableListOf<Char>()
 
     for (i in 0 until inputLengthPassword) {
-        val char = if (i % TO_EVEN_NUMBER == REMAINDER_NUMBER) numbers.random() else specialCharArray.random()
+        val char = if (i % TO_EVEN_NUMBER == REMAINDER_NUMBER) numbers.random() else symbols.random()
         passwordList.add(char)
     }
 
-    println("Ваш пароль: ${passwordList.joinToString("")}")
+    return passwordList
 }
 
 const val TO_EVEN_NUMBER = 2
