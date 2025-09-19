696c94adb1d8c7c9c251e37538309a1b05446c4c
ramoart <donni.lab.1@gmail.com>
2025-09-19 08:24:01 +0200

    KS-11-2 has been completed, please approve the pull request.

--- src/main/kotlin/lesson11/lesson11_task2.kt
+++ src/main/kotlin/lesson11/lesson11_task2.kt
@@ -13,3 +13,31 @@ fun main() {
     user1.writeBio()
     user1.changePassword()
 }
+
+class User2(
+    val id: Int,
+    val login: String,
+    var password: String,
+    val email: String,
+    var bio: String? = null
+) {
+    fun printUserData() {
+        println("Найден пользователь $login с почтой email $email")
+    }
+
+    fun writeBio() {
+        bio = readln()
+    }
+
+    fun changePassword() {
+        println("Для изменения пароля введите старый пароль:")
+        val inputPassword = readln()
+        if (inputPassword == password) {
+            println("Введите новый пароль")
+            password = readln()
+            println("Новый пароль успешно сохранен")
+        } else {
+            println("Старый пароль неверен")
+        }
+    }
+}
