682799b8f04fcd1c2db1fd7ed26ed0a43d59f5aa
ramoart <donni.lab.1@gmail.com>
2025-09-30 08:23:36 +0200

    KS-13-1 has been completed, please approve the pull request.

--- src/main/kotlin/lesson13/lesson13_task1.kt
+++ src/main/kotlin/lesson13/lesson13_task1.kt
@@ -1,18 +1,7 @@
 package lesson13
 
-fun main() {
-    val phoneBook1 = PhoneBook("ramoart", 880055555, "Google")
-    val phoneBook2 = PhoneBook("volandemort", 880055555, null)
-    phoneBook1.printData()
-    phoneBook2.printData()
-}
-
 class PhoneBook(
     val name: String,
     val phoneNumber: Long,
     val companyName: String?,
-) {
-    fun printData() {
-        println("Имя пользователя $name, телефонный номер $phoneNumber, название компании $companyName")
-    }
-}
+)
