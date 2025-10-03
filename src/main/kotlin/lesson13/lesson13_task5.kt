6026d624f9527f2f818b128f8c99c78b6d4ae449
ramoart <donni.lab.1@gmail.com>
2025-10-03 09:09:38 +0200

    KS-13-5 has been completed, please approve the pull request.

--- src/main/kotlin/lesson13/lesson13_task5.kt
+++ src/main/kotlin/lesson13/lesson13_task5.kt
@@ -3,12 +3,13 @@ package lesson13
 fun main() {
     println("Введите по очереди имя пользователя, номер телефона и название компании")
     val name = readln()
-    val inputNumber = readln()
-    val phoneNumber = inputNumber.toLongOrNull()
-    if (phoneNumber == null) {
-        println("Error: NumberFormatException")
-        return
+    var phoneNumber: Long? = null
+    try {
+        phoneNumber = readln().toLong()
+    } catch (e: Exception) {
+        println("Ошибка ввода $e")
     }
+
     val companyName = readln()
     val phoneBook = PhoneBook4(name, phoneNumber, companyName)
     val phoneBookList = mutableListOf<PhoneBook4>(phoneBook)
@@ -17,7 +18,7 @@ fun main() {
 
 class PhoneBook4(
     val name: String,
-    val phoneNumber: Long,
+    val phoneNumber: Long?,
     val companyName: String?,
 ) {
     override fun toString(): String {
