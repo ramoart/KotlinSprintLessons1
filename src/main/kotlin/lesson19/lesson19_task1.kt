6ad9943fee8f06f34c5b23c01b688a5f4e8c1a06
ramoart <donni.lab.1@gmail.com>
2025-11-06 02:34:55 -0500

    KS-19-1 has been completed, please approve the pull request.

--- src/main/kotlin/lesson19/lesson19_task1.kt
+++ src/main/kotlin/lesson19/lesson19_task1.kt
@@ -2,6 +2,7 @@ package lesson19
 
 fun main() {
     val fishList = listOf<Fish>(Fish.GUPPY, Fish.ANGELFISH, Fish.GOLDFISH, Fish.SIAMESE_FIGHTING_FISH)
+
     println("Вы можете добавить следующих рыб в свой аквариум: ${fishList.joinToString()}")
 }
 
