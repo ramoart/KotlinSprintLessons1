ac63beee5185df4e5215800ac013b1e75cde032c
ramoart <donni.lab.1@gmail.com>
2025-11-07 02:24:34 -0500

    KS-19-1 has been completed, please approve the pull request.

--- src/main/kotlin/lesson19/lesson19_task1.kt
+++ src/main/kotlin/lesson19/lesson19_task1.kt
@@ -2,13 +2,12 @@ package lesson19
 
 fun main() {
     val fishList = listOf<Fish>(Fish.GUPPY, Fish.ANGELFISH, Fish.GOLDFISH, Fish.SIAMESE_FIGHTING_FISH)
-
     println("Вы можете добавить следующих рыб в свой аквариум: " + fishList.joinToString { it.fishName })
 }
 
 enum class Fish(val fishName: String) {
     GUPPY("гуппи"),
-    ANGELFISH("скалярий"),
+    ANGELFISH("скалярия"),
     GOLDFISH("золотая рыбка"),
-    SIAMESE_FIGHTING_FISH("петушок"),
+    SIAMESE_FIGHTING_FISH("петушок")
 }
