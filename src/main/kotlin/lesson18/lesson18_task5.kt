334831e1c6da960eddce5dcd75e5476a39a84cbb
ramoart <donni.lab.1@gmail.com>
2025-11-10 01:54:53 -0500

    KS-18-3 has been completed, please approve the pull request.

--- src/main/kotlin/lesson18/lesson18_task5.kt
+++ src/main/kotlin/lesson18/lesson18_task5.kt
@@ -1,21 +1,19 @@
 package lesson18
 
 fun main() {
-    val figure = Screen(0, 0, 5, 0.0f, 0.0f, 10.0f)
+    val figure = Screen()
     println(figure.drawSquare(0, 0, 5))
     println(figure.drawSquare(0.0f, 0.0f, 10.0f))
     println(figure.drawPoint(0, 0))
     println(figure.drawCircle(0.0f, 0.0f, 10.0f))
+
+    println(figure.drawSquare(0, 0))
+    println(figure.drawSquare(0.0f, 0.0f))
+    println(figure.drawCircle(5, 5))
+    println(figure.drawPoint(1.5f, 2.5f))
 }
 
-class Screen(
-    xInt: Int,
-    yInt: Int,
-    lengthSquare: Int,
-    xFloat: Float,
-    yFloat: Float,
-    radius: Float,
-) {
+class Screen() {
     fun drawSquare(xInt: Int, yInt: Int, lengthSquare: Int): String {
         val topLeft = Pair(xInt, yInt)
         val topRight = Pair(xInt + lengthSquare, yInt)
@@ -32,12 +30,26 @@ class Screen(
         return "Нарисован квадрат (Float) в следующих координатах: $topLeft, $topRight, $bottomLeft, $bottomRight"
     }
 
+    fun drawSquare(xInt: Int, yInt: Int): String =
+        "Нарисован квадрат в координатах: x = $xInt, y = $yInt"
+
+    fun drawSquare(xFloat: Float, yFloat: Float): String =
+        "Нарисован квадрат (Float) в координатах: x = $xFloat, y = $yFloat"
+
     fun drawCircle(xFloat: Float, yFloat: Float, radius: Float): String {
         return "Нарисован круг в координатах: x = $xFloat, y = $yFloat (r = $radius)"
     }
 
+    fun drawCircle(xInt: Int, yInt: Int): String =
+        "Нарисован круг в координатах: x = $xInt, y = $yInt"
+
     fun drawPoint(xInt: Int, yInt: Int): String {
         val point = Pair(xInt, yInt)
+        return "Нарисована точка в координатах $point"
+    }
+
+    fun drawPoint(xFloat: Float, yFloat: Float): String {
+        val point = Pair(xFloat, yFloat)
         return "Нарисована точка в координатах $point"
     }
 }
