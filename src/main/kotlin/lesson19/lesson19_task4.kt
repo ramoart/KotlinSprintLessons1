537001387946709a21e6bde44f6c7f0e9e0b104f
ramoart <donni.lab.1@gmail.com>
2025-11-13 01:56:27 -0500

    KS-19-4 has been completed, please approve the pull request.

--- src/main/kotlin/lesson19/lesson19_task4.kt
+++ src/main/kotlin/lesson19/lesson19_task4.kt
@@ -1,2 +1,37 @@
 package lesson19
 
+fun main() {
+    val tank1 = Tank()
+    tank1.shoot()
+    tank1.arming(Ammunition.BLUE)
+    tank1.shoot()
+
+    tank1.arming(Ammunition.GREEN)
+    tank1.shoot()
+
+    tank1.arming(Ammunition.RED)
+    tank1.shoot()
+}
+
+enum class Ammunition(val damage: Int) {
+    BLUE(5),
+    GREEN(10),
+    RED(20)
+}
+
+class Tank {
+    private var currentAmmo: Ammunition? = null
+
+    fun arming(ammunition: Ammunition) {
+        currentAmmo = ammunition
+        println("Танк заряжен патронами: $currentAmmo")
+    }
+
+    fun shoot() {
+        if (currentAmmo == null) println("Танк пустой, зарядите патроны!")
+        else println(
+            "Танк выстрелил, нанесенный урон ${currentAmmo?.damage}, использованный тип" +
+                    " патронов $currentAmmo"
+        )
+    }
+}
