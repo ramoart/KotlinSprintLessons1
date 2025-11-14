ca93e25650709460f76fdd7df3957bfd787aff43
ramoart <donni.lab.1@gmail.com>
2025-11-14 02:39:35 -0500

    KS-20-2 has been completed, please approve the pull request.

--- src/main/kotlin/lesson20/lesson20_task2.kt
+++ src/main/kotlin/lesson20/lesson20_task2.kt
@@ -1,2 +1,16 @@
 package lesson20
 
+fun main() {
+    val player1 = Player("volandemort", 42)
+    val healing: (Player) -> Unit = { player: Player ->
+        player.currentHealth = player.maxHealth
+        println("Здоровье игрока вылечено до ${player.currentHealth}")
+    }
+    healing(player1)
+}
+
+class Player(
+    val playerName: String,
+    var currentHealth: Int,
+    val maxHealth: Int = 100,
+)
